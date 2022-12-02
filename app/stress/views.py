0018ea456094889a1e351e12a0439d4736940105
A.Kozlov <alexander.e.kozlov@mail.ru>
2022-12-02 18:22:24 +0300

    - utils: Sections update:        - div_x, div_y option added for properties recalculation;        - ISection, BaseSegment added;        - some minor changes in code; - views: function sections analysis updated from dummy to initial link with section calculation for testing;

--- app/stress/views.py
+++ app/stress/views.py
@@ -1,5 +1,7 @@
 import aiohttp_jinja2
 import asyncio
+from .utils import ISection
+
 
 @aiohttp_jinja2.template("index.html")
 async def index(request):
@@ -9,9 +11,20 @@ async def index(request):
 async def section_analysis(request):
     """
     calculate cross-section a*b with time delay = time
-    :param request: ?a=20&b=20&time=2
+    :param request: cross_section?sec_type=ISection&w1=4&h=8&w2=6&t1=2&t2=1&t3=3&x=0&y=0&alpha=0&time=0
     :return:
     """
     parameters = request.rel_url.query
     await asyncio.sleep(int(parameters['time']))
-    return {'title': f"cross-section =  {float(parameters['a']) * float(parameters['b'])}"}
+    try:
+        # print(parameters)
+        class_name = parameters['sec_type']
+        input_data = [float(_val) for _key, _val in parameters.items() if _key not in ('sec_type', 'time')]
+        # print(input_data)
+        section = globals()[class_name](*input_data)
+        return {'title': f"cross-section =  {section.area}\n cog = {section.cog}\n, inertia= {section.inertia}"}
+    except:
+        return {'title': 'something wrong'}
+
+
+
