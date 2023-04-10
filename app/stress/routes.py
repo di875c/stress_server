83ec7ebadf0533d826c05faad7f6f1295a279e6e
alexander.kozlov <alexander.e.kozlov@mail.ru>
2023-04-10 10:01:02 +0300

    - Added docker-compose folder

--- app/stress/routes.py
+++
@@ -1,6 +0,0 @@
-from app.stress import views
-
-# настраиваем пути, которые будут вести к нашей странице
-def setup_routes(app):
-   app.router.add_get("/", views.index)
-   app.router.add_route('*', "/db", views.DbView)
